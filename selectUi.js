bcb5699e29ce9761e67c0eb5270d00c0ae263748
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-01-03 18:23:15 +0800

    优化了菜单选项对象

--- selectUi.js
+++ selectUi.js
@@ -3,21 +3,18 @@ const selectAll = require("./selectAll");
 
 async function selectUI() {
   const options = {
-    "1": "列出全部商品信息",
-  };
-  const menu = {
-    "1": selectAll,
+    "1": ["列出全部商品信息",selectAll],
   };
 
   for (let [k, v] of Object.entries(options)) {
-    console.log("按下",k, "\t", v);
+    console.log("按下",k, "\t", v[0]);
   }
   console.log("如果选择无效 则默认列出全部商品信息");
   const select = await input("请输入你的选项: ");
-  if (menu[select] === undefined) {
-    menu["1"]();
+  if (options[select] === undefined) {
+    options["1"][1]();
   } else {
-    menu[select]();
+    options[select][1]();
   }
 }
 
