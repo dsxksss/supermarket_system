9fe7e348f36c7569f540eb0145451e779904fec6
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-01-12 22:33:42 +0800

    实现支付功能 项目v: 1.0.0

--- router/options/uiOption.js
+++ router/options/uiOption.js
@@ -2,12 +2,14 @@ const addUI = require("../../ui/addUi");
 const selectUI = require("../../ui/selectUI");
 const changeUI = require("../../ui/changeUI");
 const deleteUI = require("../../ui/deleteUI");
+const pay = require("../../functions/pay");
 
 const uiOption = {
   "录入信息": addUI,
   "查找信息": selectUI,
   "修改信息": changeUI,
   "删除信息": deleteUI,
+  "购买并支付商品": pay,
   "退出程序":process.exit
 };
 
