badbadafb1229ced28fd7d056a3e07e63f20740e
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-05-22 23:16:06 +0800

    改用prisma进行数据库的读写

--- functions/deleteAll.js
+++ functions/deleteAll.js
@@ -1,4 +1,3 @@
-const connection = require("../db/connection");
 const input = require("scanline");
 
 async function deleteAll() {
@@ -7,17 +6,12 @@ async function deleteAll() {
     "\033[31m你确定要删除全部商品信息吗!?[y确认/n取消]\033[0m",
   );
   if (yn === "y" || yn === "Y") {
-    connection.query("truncate table product", (error, _) => {
-      if (error) {
-        console.log("删除全部商品信息失败", error);
-        process.exit(1);
-      }
-      console.log("\033[32m你已删除全部商品信息\033[0m");
-    });
+    const result = await prismaClient.product.deleteMany()
+    console.log(`${"\033[32m"}共删除${result.count}个商品${"\033[0m"}`);
+    console.log("\033[32m你已删除全部商品信息\033[0m");
   } else {
     console.log("\033[32m你已取消该操作\033[0m");
   }
-  connection.end();
 }
 
 module.exports = deleteAll;
--- functions/deleteOne.js
+++ functions/deleteOne.js
@@ -1,20 +1,14 @@
-const connection = require("../db/connection");
 const input = require("scanline");
 const checkProductExist = require("./checkProductExist");
 
 async function deleteOne() {
   showPath();
   const name = await input("请输入你想要删除的商品名称: ");
-  checkProductExist(name);
-
-  connection.query("delete from product where 名称=?", [name], (error, _) => {
-    if (error) {
-      console.log("删除单个商品失败", error);
-      process.exit(1);
-    }
-    console.log("\033[32m商品删除成功\033[0m");
-  });
-  connection.end();
+  await checkProductExist(name);
+  const result = await prismaClient.product.delete({
+    where: { name }
+  })
+  console.log(`${"\033[32m"}商品${result.name}删除成功${"\033[0m"}`);
 }
 
 module.exports = deleteOne;
